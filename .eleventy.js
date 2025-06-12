ff1028c81a798f90439491dca1f3ad2d36155c61
mads-bohn <mady.bohn@gmail.com>
2025-06-12 12:57:00 -0500

    eleventy config and gitignore

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+module.exports = function (eleventyConfig) {
+
+  // Stop the default behaviour of foo.html being turned into foo/index.html
+  eleventyConfig.addGlobalData("permalink", "{{ page.filePathStem }}.html");
+
+  // Copy the assets folder to the output without modifying it at all
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  // Define which file types will be copied
+  eleventyConfig.setTemplateFormats(["html", "njk", "txt", "js", "css", "xml", "json"]);
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
+};
