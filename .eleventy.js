9c3b71ace1d546986aa80975bc083c89857ce9ed
mads-bohn <mady.bohn@gmail.com>
2025-06-17 16:05:09 -0500

    changed 11ty config to still look at files ignored by git

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,9 @@ module.exports = function (eleventyConfig) {
   // Copy the assets folder to the output without modifying it at all
   eleventyConfig.addPassthroughCopy("src/assets");
 
+  // tells 11ty to still look at files in gitignore
+  eleventyConfig.setUseGitIgnore(false);
+
   // Define which file types will be copied
   eleventyConfig.setTemplateFormats(["html", "njk", "txt", "js", "css", "xml", "json"]);
 
